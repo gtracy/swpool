2a540c1669643782b1c0bdb26cd350e2a98c8e07
Greg Tracy <gtracy@gmail.com>
2023-06-25 14:03:30 -0500

    implemented a dev switch to disable analytics

--- src/analytics.js
+++ src/analytics.js
@@ -1,15 +1,25 @@
 import ReactGA from 'react-ga4';
+
+const DEV_SWITCH = true;
 ReactGA.initialize('G-ETL4FMMKE5');
 
-const buttonClick = (action) => ReactGA.event({
-    category: "button click",
-    action: action
-})
+const buttonClick = (action) => {
+    if( !DEV_SWITCH ) {
+        ReactGA.event({
+            category: "button click",
+            action: action
+        })
+    }
+};
 
-const eventOccurred = (event) => ReactGA.event({
-    category: "event occurred",
-    action: event
-})
+const eventOccurred = (event) => {
+    if( !DEV_SWITCH ) {
+        ReactGA.event({
+            category: "event occurred",
+            action: event
+        })
+    }
+}
 
 const gaEvents = {
     buttonClick: buttonClick,
